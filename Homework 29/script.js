9dad8017b45eaa72cda3b60452005f060e43c67b
KirichukLudmila <119871450+KirichukLudmila@users.noreply.github.com>
2023-02-19 14:39:37 +0200

    add Homework_29

---
+++ Homework 29/script.js
@@ -0,0 +1,13 @@
+let name = prompt(" your name is");
+alert("Привіт,"+" "+"name");
+
+
+const currentYear = 2023;
+let userYear =+prompt('year of birth');
+let userAge= currentYear-userYear;
+alert("Your age is" +" "+ userAge);
+
+
+let squareSide = +prompt("the length of square's side is');
+let perimeter = squareSide*4;
+alert("Perimeter of the square is" +" "+ perimeter);
